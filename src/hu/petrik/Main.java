fd116b46478648162f459c6a23d722ff94c49e6f
Kovács Krisztián <k.krisztian0830@gmail.com>
2021-09-27 11:57:52 +0200

    Új festmények felvétele megadott darabszám alapján, Fájl beolvasás

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,5 +1,8 @@
 package hu.petrik;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,6 +16,44 @@ public class Main {
         Festmeny f2=new Festmeny("Elfolyó idő", "Salvador Dalí", "szürrealista");
         festmenyLista.add(f1);
         festmenyLista.add(f2);
+        UjFestmenyHozzaAdas();
+        festmenyLista.addAll(fajlBeolvas("festmenyek.csv"));
+        //System.out.println(festmenyLista);
 
+
+    }
+    public static void UjFestmenyHozzaAdas(){
+        System.out.println("Adjon meg darabszámot:");
+        int darabSzam= sc.nextInt();
+        for (int i = 0; i < darabSzam; i++) {
+            System.out.println("Mi a festmény cime?");
+            String cim=sc.next();
+            System.out.println("Ki festette?");
+            String festo=sc.next();
+            System.out.println("Milyen stilusban?");
+            String stilus=sc.next();
+
+            festmenyLista.add(new Festmeny(cim, festo, stilus));
+        }
+    }
+
+    public static  List<Festmeny> fajlBeolvas(String fajlNev){
+        ArrayList<Festmeny> festmenyLista = new ArrayList<>();
+        try {
+            FileReader fr=new FileReader(fajlNev);
+            BufferedReader br=new BufferedReader(fr);
+            String sor= br.readLine();
+            while (sor!=null){
+                String[] adatok=sor.split(";");
+                Festmeny festmeny= new Festmeny(adatok[1],adatok[0],adatok[2]);
+                festmenyLista.add(festmeny);
+                sor= br.readLine();
+            }
+            br.close();
+            fr.close();
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        return festmenyLista;
     }
 }
