80ecc3ca5d014f993a3609e01529de39a03aa381
Kovács Krisztián <k.krisztian0830@gmail.com>
2021-09-27 10:33:44 +0200

    Lista létrehozása, Két festmény felvétele a listába

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,8 +1,18 @@
 package hu.petrik;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc=new Scanner(System.in);
+    static List<Festmeny> festmenyLista=new ArrayList<Festmeny>();
 
     public static void main(String[] args) {
-	// write your code here
+        Festmeny f1=new Festmeny("Csillagos éj", "Vincent van Gogh", "posztimpresszionizmus");
+        Festmeny f2=new Festmeny("Elfolyó idő", "Salvador Dalí", "szürrealista");
+        festmenyLista.add(f1);
+        festmenyLista.add(f2);
+
     }
 }
